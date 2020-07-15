15c4adccb2a2b4308fc6cacc4a51b9c7b9d4b33c
Gurudutt Suryanarayana <gnsuryan@users.noreply.github.com>
2020-07-15 20:24:50 +0530

    Update basic_test.sh

--- basic_test.sh
+++ basic_test.sh
@@ -1,209 +1,242 @@
 #!/bin/bash
 
-CURR_DIR=`pwd`
+CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-source $PWD/test_config.properties
+source $CURR_DIR/utils.sh
 
-
-function getHostInfo()
+function testHostInfo()
 {
+    startTest
+
     hostnamectl
-}
 
-function startTest()
-{
-    TEST_INFO="$1"
-    echo "\n\n"
-    echo " =========================================================================================="
-    echo " EXECUTION START:  TEST >>>>>>     ${TEST_INFO}      <<<<<<<<<<<<<<<<<<<"
-}
+    hostnamectl | grep "$OS_VERSION"
 
-function endTest()
-{
-    TEST_INFO="$1"
-    echo " EXECUTION END :    TEST >>>>>>     ${TEST_INFO}      <<<<<<<<<<<<<<<<<<<"
-    echo " =========================================================================================="
-    echo "\n\n"
-}
+    if [ "$?" != "0" ];
+    then
+       echo "FAILURE - VM: OS Version verification failed. "
+       notifyFail
+    else
+       echo "SUCCESS - VM: OS version verified successfully"
+       notifyPass
+    fi
 
+    endTest
+}
 
 function testWLSInstallPath()
 {
-    startTest "testWLSInstallPath"
+    startTest
+
+    echo "WLS_HOME: ${WLS_HOME}"
 
     if [ ! -d "${WLS_HOME}" ]; then
-      echo "Weblogic Server not installed as per the expected directory structure"
+      echo "Weblogic Server not installed as per the expected directory structure: ${WLS_HOME} "
+      notifyFail
     else
       echo "Weblogic Server install path verified successfully"
+      notifyPass
     fi
 
-    endTest "testWLSInstallation"
-
+    endTest
 }
 
 function testWLSVersion()
 {
-    startTest "testWLSInstallPath"
-    
+    startTest
+
+    if [ ! -d "${WLS_HOME}" ]; then
+      echo "Weblogic Server not installed as per the expected directory structure"
+      notifyFail
+    else
+        
+        cd ${WLS_HOME}/server/bin
+
+        . ./setWLSEnv.sh
+
+        OUTPUT="$(java weblogic.version)"
+        echo "${OUTPUT}"
+
+        echo "${OUTPUT}"|grep ${WLS_VERSION}
+
+        if [ "$?" != "0" ];
+        then
+           echo "FAILURE - Weblogic Server Version could not be verified "
+           notifyFail
+        else
+           echo "SUCCESS - Weblogic Server Version verified successfully"
+           notifyPass
+        fi
+    fi
+
+    endTest "testWLSVersion"
+
+}
+
+function testJavaInstallPath()
+{
+    startTest
+
     cd ${WLS_HOME}/server/bin
 
     . ./setWLSEnv.sh
 
-    OUTPUT="$(java weblogic.version)"
-    echo "${OUTPUT}"
-
-    echo "${OUTPUT}"|grep ${WLS_VERSION}
-
-    if [ "$?" != "0" ];
-    then
-       echo "FAILURE - Weblogic Server Version could not be verified "    
+    if [ ! -d "${JAVA_HOME}" ]; then
+      echo "JAVA/JDK is not installed as per the expected directory structure: ${WLS_HOME} "
+      notifyFail
     else
-       echo "SUCCESS - Weblogic Server Version verified successfully"
-    fi
+      echo "JAVA/JDK installation path verified successfully"
+      notifyPass
+    fi     
 
-    endTest "testWLSInstallPath"
-
+    endTest
 }
 
-function testJavaInstallation()
+function testJavaVersion()
 {
-    startTest "testJavaInstallation"
+    startTest
 
-     if type -p java; then
-        echo found java executable in PATH
-        _java=java
-    elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
-        echo found java executable in JAVA_HOME     
-        _java="$JAVA_HOME/bin/java"
-    else
-        echo "no java"
-    fi
+    cd ${WLS_HOME}/server/bin
 
-    if [[ "$_java" ]]; then
-        version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
-        echo version "$version"
-    fi
+    . ./setWLSEnv.sh
 
-    echo "${version}"|grep ${JDK_VERSION}
+    java -version 2> /tmp/java_version.txt
 
+    cat /tmp/java_version.txt 
+    
+    cat /tmp/java_version.txt |grep "${JDK_VERSION}"
+    
     if [ "$?" != "0" ];
     then
-       echo "FAILURE - Java Version could not be verified "    
+       echo "FAILURE - Java Version could not be verified "
+       notifyFail
     else
        echo "SUCCESS - Java Server Version verified successfully"
+       notifyPass
     fi
 
-    endTest "testJavaInstallation"
+    rm -f /tmp/java_version.txt
+
+    endTest
 }
-
-function testDomainSetup()
-{
-
-    if [ ! -d "${DOMAIN_DIR}" ]; then
-      echo "Weblogic Domain is not available as expected"
-      exit 1
-    fi
-}
-
 
 function testJDBCDrivers()
 {
 
-    startTest "testJavaInstallation"
+    startTest
 
     cd ${WLS_HOME}/server/bin
 
-    . ./setWLSEnv.sh
+    . ./setWLSEnv.sh >/dev/null
   
-    echo ${CLASSPATH}
-
     echo ${CLASSPATH} | grep "${POSTGRESQL_JAR}"
 
     if [ $? == 1 ];
     then
         echo "FAILURE - ${POSTGRESQL_JAR} file is not found in Weblogic Classpath as expected"
+        notifyFail
     else
         echo "SUCCESS - ${POSTGRESQL_JAR} file found in Weblogic Classpath as expected"
+        notifyPass
     fi
+
+    echo "==========================================================================="
 
     echo $CLASSPATH|grep "${MSSQL_JAR}"
 
     if [ $? == 1 ];
     then
         echo "FAILURE - ${MSSQL_JAR} file is not found in Weblogic Classpath as expected"
+        notifyFail
     else
         echo "SUCCESS - ${MSSQL_JAR} file found in Weblogic Classpath as expected"
+        notifyPass
     fi
 
-    endTest "testJavaInstallation"
+    endTest
 }
 
-
-function testAdminConsoleHTTP()
+function testWDTInstallation()
 {
-    retcode=$(curl -s -o /dev/null -w "%{http_code}" ${HTTP_ADMIN_URL} )
 
-    if [ "${retcode}" != "200" ];
+    if [ ! -d "$WDT_HOME" ];
     then
-        echo "Error!! Admin Conosle is not accessible. Curl returned code ${retcode}"
+        echo "FAILURE: Weblogic Deploy Tool not found"
+        notifyFail
+        endTest
+        return
+    else
+        echo "SUCCESS: Weblogic Deploy Tool found"
+        notifyPass
+        
+        $WDT_HOME/bin/createDomain.sh
+
+        if [ "$?" != "0" ];
+        then
+            echo "FAILURE: Failed to verify Deploy Tool"
+            notifyFail
+        else
+            echo "SUCCESS: Deploy tool verified successfully"
+            notifyPass
+        fi
     fi
+
+    endTest    
 }
 
-function testAdminConsoleHTTPS()
+function testRNGDService()
 {
-    retcode=$(curl --insecure -s -o /dev/null -w "%{http_code}" ${HTTPS_ADMIN_URL})
 
-    if [ "${retcode}" != "200" ];
+    startTest
+
+    systemctl status rngd | grep "active (running)"
+
+    if [ "$?" != "0" ];
     then
-        echo "Error!! Admin Conosle is not accessible. Curl returned code ${retcode}"
+        echo "FAILURE: rngd service not active"
+        notifyFail
+        endTest
+        return
+    else
+        echo "SUCCESS: rngd service is active"
+        notifyPass
     fi
+
+    endTest
+
 }
 
-function getServerStatus()
+function testUtilities()
 {
+    isUtilityInstalled "zip"
 
-    curl -v \
-    --user ${WLS_USERNAME}:${WLS_PASSWORD} \
-    -H X-Requested-By:MyClient \
-    -H Accept:application/json \
-    -X GET http://localhost:7001/management/weblogic/latest/domainRuntime/serverLifeCycleRuntimes?links=none&fields=name,state
+    isUtilityInstalled "unzip"
 
+    isUtilityInstalled "wget"
+
+    isUtilityInstalled "rng"
+
+    isUtilityInstalled "cifs-utils"
 }
-
-function testAppDeployment()
-{
-    curl -v \
-    --user ${WLS_USERNAME}:${WLS_PASSWORD} \
-    -H X-Requested-By:MyClient \
-    -H Accept:application/json \
-    -H Content-Type:application/json \
-    -d "{
-     name: '${DEPLOY_APP_NAME}',
-     applicationPath : '/apps/oracle-weblogic/applications/mwiapp.war',
-     targets: ['mwiCluster1'],
-     plan: '/apps/oracle-weblogic/applications/plan.xml',
-     deploymentOptions: {}
-    }" \
-    -X POST http://localhost:17001/management/weblogic/latest/domainRuntime/deploymentManager/deploy_
-}
-
 
 #main
 
-getHostInfo
+testHostInfo
 
-testWLSInstallation
+testWLSInstallPath
 
-testJavaInstallation
+testWLSVersion
+
+testJavaInstallPath
+
+testJavaVersion
 
 testJDBCDrivers
 
-#testDomainSetup
+testWDTInstallation
 
-#testAdminConsoleHTTP
+testRNGDService
 
-#testAdminConsoleHTTPS
+testUtilities
 
-#testServerStatus
-
-#testAppDeployment
+printTestSummary

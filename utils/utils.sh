e950e96eb4e4b369ce174bbafc7e717120b35dc0
gnsuryan <gurudutt.suryanarayana@oracle.com>
2022-05-05 10:34:06 +0000

    added ssl test cases

--- utils/utils.sh
+++ utils/utils.sh
@@ -10,7 +10,12 @@ usage()
 cat << USAGE >&2
 Usage:
     -i            INPUT_FILE        Path to Command input File
+    -r            RG_NAME           Resource Group Name (Optional)
+    -o            OTHER_ARGS        Comma separated list of arguments in keyvalue pair (Optional)
     -h|?|--help   HELP              Help/Usage info
+
+ex: $0 -i /test_input/OL7.6_14.1.1.0.0_JDK8.props -r MyResourceGrp
+
 USAGE
 
 exit 1
@@ -31,7 +36,6 @@ function print()
   fi
 }
 
-
 get_param()
 {
     while [ "$1" ]
@@ -39,12 +43,27 @@ get_param()
         case $1 in
          -i         )  INPUT_FILE=$2 ;;
          -r         )  RG_NAME=$2;;
+         -o         )  OTHER_ARGS=$2;;
                    *)  echo 'invalid arguments specified'
                        usage;;
         esac
         shift 2
     done
 }
+
+read_other_args()
+{
+  if [ ! -z "$OTHER_ARGS" ];
+  then
+    IFS=','
+    read -ra ARGS <<< "$OTHER_ARGS"
+    for i in "${ARGS[@]}";
+    do
+        eval "export $i"
+    done
+  fi
+}
+
 
 validate_input()
 {
@@ -65,8 +84,15 @@ validate_input()
         echo "command input RG_NAME not provided"
     fi
 
+    if [[ -z "$OTHER_ARGS" ]];
+    then
+       echo "command input OTHER_ARGS not provided"
+    fi
+
     echo "Using input file $INPUT_FILE"
     source $INPUT_FILE
+
+    read_other_args
 }
 
 function notifyPass()
@@ -205,4 +231,7 @@ source ${UTILS_DIR}/test_config.properties
 
 export passcount=0
 export failcount=0
+
+
+
 
